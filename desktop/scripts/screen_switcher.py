abe9cff59d10b71ca2cbd2cac5a41bd64dd884e6
Vlad Chepaikyn <backdround@yandex.ru>
2019-06-06 00:26:05 +0300

    i3. Categorize workspaces

--- desktop/scripts/screen_switcher.py
+++ desktop/scripts/screen_switcher.py
@@ -4,9 +4,9 @@ import json
 from itertools import cycle
 
 workspaces_info = json.loads(subprocess.getoutput("i3-msg -t get_workspaces"))
-workspaces = [(i["num"], i["focused"]) for i in workspaces_info if i["visible"]]
+workspaces = [(i["output"], i["focused"]) for i in workspaces_info if i["visible"]]
 
-# get next index
+# get index to switch
 for i in workspaces:
     if i[1]:
         if i == workspaces[-1]:
@@ -15,7 +15,5 @@ for i in workspaces:
             index = workspaces.index(i) + 1
         break
 
-switch_command = "i3-msg workspace {}".format(workspaces[index][0])
-print(workspaces)
-print(switch_command)
+switch_command = "i3-msg focus output {}".format(workspaces[index][0])
 subprocess.getoutput(switch_command)
