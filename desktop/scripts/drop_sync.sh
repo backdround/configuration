f5bea73d280e20d5c65a23cc1e45e9846840be9c
Vlad Chepaykyn <backdround@yandex.ru>
2019-06-26 22:45:07 +0300

    drop. Create backup on startup

--- desktop/scripts/drop_sync.sh
+++ desktop/scripts/drop_sync.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# create backup only when first run
+BACKUP_FLAG=/tmp/drop_backup
+if [ ! -f $BACKUP_FLAG ] ; then
+  touch $BACKUP_FLAG
+
+  # remove older backup
+  rm -rf ~/.local/share/drop
+  mkdir -p ~/.local/share/
+
+  cp -rf ~/drop ~/.local/share/
+fi
+
+# sync notes
 if ! `rclone check drop: ./drop/ 2> /dev/null` ; then
   rclone sync drop: ~/drop/
 fi
