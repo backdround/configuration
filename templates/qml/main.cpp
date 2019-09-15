4f1be997187199194fd9cb5d1a64ba1d7183e4cd
Vlad Chepaykyn <backdround@yandex.ru>
2019-09-15 11:41:09 +0300

    template. add qml

---
+++ templates/qml/main.cpp
@@ -0,0 +1,8 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine("main.qml");
+    return app.exec();
+}
---
+++ templates/qml/main.pro
@@ -0,0 +1,4 @@
+TEMPALTE = app
+QT = core gui widgets qml
+CONFIG += debug c++17
+SOURCES += main.cpp
