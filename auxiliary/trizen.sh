1ca6bfee581ac97bd8f55b01899b31de3638f112
Vlad Chepaykyn <backdround@yandex.ru>
2020-09-01 21:59:07 +0300

    trizen. ignore continuously changes in trizen config

--- auxiliary/trizen.sh
+++ auxiliary/trizen.sh
@@ -6,3 +6,7 @@ git clone https://aur.archlinux.org/trizen.git
 cd trizen/
 makepkg --needed --install --noconfirm --syncdeps
 sed 's~^\(.*clone_dir.*\)".*"\(.*\)~\1"$ENV{HOME}/.tmp/trizen"\2~' -i ~/.config/trizen/trizen.conf
+
+# Ignore further changes in trizen config.
+TRIZEN_PATH=$(find . -name "trizen.conf")
+git update-index --assume-unchanged "$TRIZEN_PATH"
