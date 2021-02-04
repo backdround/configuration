d581c7778c97a5b164311b82c6e735343a4dec6e
Vlad Chepaykyn <backdround@yandex.ru>
2021-02-05 01:58:02 +0300

    install. fix scripts

--- auxiliary/trizen.sh
+++ auxiliary/trizen.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Install trizen package
 
+# Install trizen package
 cd /tmp/
 git clone https://aur.archlinux.org/trizen.git
 cd trizen/
 makepkg --needed --install --noconfirm --syncdeps
+
+# Generate config.
+trizen -q
+
+# Change tmp dir.
 sed 's~^\(.*clone_dir.*\)".*"\(.*\)~\1"$ENV{HOME}/.tmp/trizen"\2~' -i ~/.config/trizen/trizen.conf
 
 # Ignore further changes in trizen config.
