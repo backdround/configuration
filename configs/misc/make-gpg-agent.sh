c371956b144189ab3d7ef19148a637b761060de6
Vlad Chepaykyn <backdround@yandex.ru>
2023-01-18 20:03:45 +0300

    gpg-agent. fix

---
+++ configs/misc/make-gpg-agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -euo pipefail
+
+CONFIG="${1:-}"
+
+test -n "$CONFIG" || {
+  echo "Config path isn't set"
+  exit 1
+}
+
+# Sets directory permition
+CONFIG_DIRECTORY="$(dirname "$(realpath "$CONFIG")")"
+chmod 700 "$CONFIG_DIRECTORY"
+
+# Creates config
+cat > "$CONFIG" <<EOF
+default-cache-ttl 30
+max-cache-ttl 60
+EOF
+
+# Sets config permition
+chmod 600 "$CONFIG"
