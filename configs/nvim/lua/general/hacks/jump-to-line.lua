d2a0c6398cf3937f03dd048fbdba9cfe05fea838
Vlad Chepaykyn <backdround@yandex.ru>
2023-10-07 06:42:18 +0300

    nvim. overhaul jump-to-line module

--- configs/nvim/lua/general/hacks/jump-to-line.lua
+++ configs/nvim/lua/general/hacks/jump-to-line.lua
@@ -6,8 +6,14 @@
 ------------------------------------------------------------
 -- Utility functions
 
-local function is_operator_pending_mode()
-  return vim.fn.mode(true):find("o") ~= nil
+local function mode()
+  local m = vim.fn.mode(true)
+  if m:find("o") then
+    return "operator-pending"
+  elseif m:find("[vV]") then
+    return "visual"
+  end
+  return "normal"
 end
 
 local function line(line_index)
@@ -50,10 +56,76 @@ local function last_symbol_index(line_index)
   return line(line_index):len()
 end
 
+local function normalize(line_index, column_index)
+  if line_index < 1 then
+    return 1, 0
+  end
+
+  local last_line = vim.fn.line("$")
+  if line_index > last_line then
+    return last_line, last_symbol_index(last_line)
+  end
+
+  if column_index < 0 then
+    return line_index, 0
+  end
+
+  local last_column = last_symbol_index(line_index)
+  if column_index > last_column then
+    return line_index, last_column
+  end
+
+  return line_index, column_index
+end
+
+local function move_back_once(line_index, column_index)
+  line_index, column_index = normalize(line_index, column_index)
+
+  if line_index == 1 and column_index == 0 then
+    return 1, 0
+  end
+
+  if column_index == 0 then
+    return line_index - 1, last_symbol_index(line_index - 1)
+  end
+
+  return line_index, column_index - 1
+end
+
 local function select_region(row1, column1, row2, column2)
   vim.api.nvim_buf_set_mark(0, "<", row1, column1, {})
   vim.api.nvim_buf_set_mark(0, ">", row2, column2, {})
   vim.cmd("normal! gv")
+end
+
+local function perform_motion(target_line, target_column)
+  target_line, target_column = normalize(target_line, target_column)
+  local initial_line = current_line_index()
+  local initial_column = current_column_index()
+
+  if mode() ~= "operator-pending" then
+    vim.api.nvim_win_set_cursor(0, { target_line, target_column })
+    return
+  end
+
+  if initial_line == target_line and initial_column == target_column then
+    return
+  end
+
+  local forward
+  if initial_line ~= target_line then
+    forward = target_line > initial_line
+  else
+    forward = target_column > initial_column
+  end
+
+  if forward then
+    target_line, target_column = move_back_once(target_line, target_column)
+  else
+    initial_line, initial_column = move_back_once(initial_line, initial_column)
+  end
+
+  select_region(initial_line, initial_column, target_line, target_column)
 end
 
 ------------------------------------------------------------
@@ -62,125 +134,37 @@ end
 local function jump_to_start_of_previous_line()
   local target_line = backward_line_index(vim.v.count1)
   local target_column = first_symbol_index(target_line)
-
-  if not is_operator_pending_mode() then
-    vim.api.nvim_win_set_cursor(0, { target_line, target_column })
-    return
-  end
-
-  local initial_column = current_column_index()
-  local initial_line = current_line_index()
-
-  if initial_line == 1 then
-    return
-  end
-
-  if initial_column == 0 then
-    initial_line = initial_line - 1
-    initial_column = last_symbol_index(initial_line)
-  else
-    initial_column = initial_column - 1
-  end
-
-  select_region(initial_line, initial_column, target_line, target_column)
+  perform_motion(target_line, target_column)
 end
-
 
 local function jump_to_end_of_previous_line()
   local target_line = backward_line_index(vim.v.count1)
   local target_column = last_symbol_index(target_line)
-
-  if not is_operator_pending_mode() then
-    vim.api.nvim_win_set_cursor(0, { target_line, target_column })
-    return
-  end
-
-  local initial_column = current_column_index()
-  local initial_line = current_line_index()
-
-  if initial_line == 1 then
-    return
-  end
-
-  if initial_column == 0 then
-    initial_line = initial_line - 1
-    initial_column = last_symbol_index(initial_line)
-  else
-    initial_column = initial_column - 1
-  end
-
-  select_region(initial_line, initial_column, target_line, target_column)
+  perform_motion(target_line, target_column)
 end
 
 local function jump_to_start_of_next_line()
   local target_line = forward_line_index(vim.v.count1)
   local target_column = first_symbol_index(target_line)
-
-  if not is_operator_pending_mode() then
-    vim.api.nvim_win_set_cursor(0, { target_line, target_column })
-    return
-  end
-
-  local initial_column = current_column_index()
-  local initial_line = current_line_index()
-
-  if initial_line == vim.fn.line("$") then
-    return
-  end
-
-  if target_column == 0 then
-    target_line = target_line - 1
-    target_column = last_symbol_index(target_line)
-  else
-    target_column = target_column - 1
-  end
-
-  select_region(initial_line, initial_column, target_line, target_column)
+  perform_motion(target_line, target_column)
 end
 
 local function jump_to_end_of_next_line()
   local target_line = forward_line_index(vim.v.count1)
   local target_column = last_symbol_index(target_line)
-
-  if not is_operator_pending_mode() then
-    vim.api.nvim_win_set_cursor(0, { target_line, target_column })
-    return
-  end
-
-  local initial_column = current_column_index()
-  local initial_line = current_line_index()
-
-  if initial_line == vim.fn.line("$") then
-    return
-  end
-
-  if last_symbol_index(target_line) == 0 then
-    target_line = target_line - 1
-    target_column = last_symbol_index(target_line)
-  else
-    target_column = target_column - 1
-  end
-
-  select_region(initial_line, initial_column, target_line, target_column)
+  perform_motion(target_line, target_column)
 end
 
 local function jump_to_start_of_current_line()
   local target_line = current_line_index()
   local target_column = first_symbol_index(target_line)
-  vim.api.nvim_win_set_cursor(0, { target_line, target_column })
+  perform_motion(target_line, target_column)
 end
 
 local function jump_to_end_of_current_line()
-  local initial_line = current_line_index()
-  local initial_column = current_column_index()
-  local target_column = last_symbol_index(initial_line)
-
-  if not is_operator_pending_mode() then
-    vim.api.nvim_win_set_cursor(0, { initial_line, target_column })
-    return
-  end
-
-  select_region(initial_line, initial_column, initial_line, target_column - 1)
+  local target_line = current_line_index()
+  local target_column = last_symbol_index(target_line)
+  perform_motion(target_line, target_column)
 end
 
 return {
