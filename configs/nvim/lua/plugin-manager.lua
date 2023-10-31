18008d65076b84d1ae3be6018aa852641b147b3a
Vlad Chepaykyn <backdround@yandex.ru>
2023-10-31 20:06:06 +0300

    nvim. set lazy.nvim mappings

--- configs/nvim/lua/plugin-manager.lua
+++ configs/nvim/lua/plugin-manager.lua
@@ -47,10 +47,36 @@ local function load()
   ensure_lazy_presence()
 
   -- Change default mappings
-  local config = require("lazy.view.config")
-  config.commands.update.key = "<M-U>"
-  config.commands.update.key_plugin = "<M-u>"
-  config.keys.diff = "<M-d>"
+  local leader = vim.api.nvim_replace_termcodes("<leader>", true, false, true)
+  local keys = require("lazy.view.config").keys
+  local commands = require("lazy.view.config").commands
+
+  keys.hover          = leader .. "/"
+  keys.diff           = leader .. "d"
+  keys.close          = "<M-s>"
+  keys.details        = "<M-o>"
+  keys.profile_sort   = leader .. "<C-s>"
+  keys.profile_filter = leader .. "<C-f>"
+  keys.abort          = "<C-c>"
+
+  commands.home.key           = "Y"
+  commands.install.key        = leader .. "<M-i>"
+  commands.install.key_plugin = leader .. "i"
+  commands.update.key         = leader .. "<M-u>"
+  commands.update.key_plugin  = leader .. "u"
+  commands.sync.key           = leader .. "s"
+  commands.clean.key          = leader .. "<M-x>"
+  commands.clean.key_plugin   = leader .. "x"
+  commands.check.key          = leader .. "<M-c>"
+  commands.check.key_plugin   = leader .. "c"
+  commands.log.key            = leader .. "<M-l>"
+  commands.log.key_plugin     = leader .. "l"
+  commands.restore.key        = leader .. "<M-r>"
+  commands.restore.key_plugin = leader .. "r"
+  commands.profile.key        = leader .. "p"
+  commands.debug.key          = leader .. "D"
+  commands.help.key           = "<C-_>" -- <C-/>
+  commands.build.key_plugin   = leader .. "b"
 
   local lazy_options = {
     install = {
