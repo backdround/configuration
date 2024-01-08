3ce9db192ac58e8e300d00f5d237b01fee9c1fe0
Vlad Chepaykyn <backdround@yandex.ru>
2024-01-08 16:34:49 +0300

    nvim. refactor mapping hijacking

--- configs/nvim/lua/utilities/mappings.lua
+++ configs/nvim/lua/utilities/mappings.lua
@@ -1,35 +1,40 @@
 local M = {}
 
----Hijacks vim.set.keymap and allows only buffer-local mappings.
+---Hijacks vim.set.keymap and allows only buffer-local, <plug> or own mappings.
 ---Also logs all attempts to mappings.log
 M._hijack_set_keymap = function()
   M._nvim_set_keymap = vim.api.nvim_set_keymap
   M._vim_keymap_set = vim.keymap.set
 
-  local mappings_log = vim.fn.stdpath("log") .. "/mappings.log"
-  os.remove(mappings_log)
-
   vim.api.nvim_set_keymap = function() end
   vim.keymap.set = function(mode, lhs, rhs, opts)
-    local plug = lhs:find("<plug>") ~= nil or lhs:find("<Plug>") ~= nil
-    if opts and opts.buffer or plug then
-      M._perform_keymap_set(mode, lhs, rhs)
-    else
-      local log = io.open(mappings_log, "a")
-      if log ~= nil then
-        local entry =
-          vim.inspect({ mode = mode, lhs = lhs, rhs = rhs, opts = opts })
-        log:write( entry .. "\n\n")
-        io.close(log)
-      end
-    end
-  end
+    local plug_mapping = lhs:find("<plug>") ~= nil or lhs:find("<Plug>") ~= nil
+    local buffer_mapping = opts and opts.buffer
+    local own_mapping = opts and opts.own
 
-  M._perform_keymap_set = function(...)
-    vim.api.nvim_set_keymap = M._nvim_set_keymap
-    M._vim_keymap_set(unpack({ ... }))
-    vim.api.nvim_set_keymap = function() end
+    if opts then
+      opts.own = nil
+    end
+
+    if buffer_mapping or plug_mapping or own_mapping then
+      vim.api.nvim_set_keymap = M._nvim_set_keymap
+      M._vim_keymap_set(mode, lhs, rhs, opts)
+      vim.api.nvim_set_keymap = function() end
+      return
+    end
+
+    M._log(vim.inspect({ mode = mode, lhs = lhs, rhs = rhs, opts = opts }))
   end
+end
+
+---@param entry string
+M._log = function(entry)
+  local log_file = io.open(M._log_file, "a")
+  if log_file == nil then
+    return
+  end
+  log_file:write(entry .. "\n\n")
+  io.close(log_file)
 end
 
 M.init = function()
@@ -39,6 +44,10 @@ M.init = function()
   M._inited = true
 
   vim.keymap.del({ "n", "x" }, "gx")
+
+  M._log_file = vim.fn.stdpath("log") .. "/mappings.log"
+  os.remove(M._log_file)
+
   M._hijack_set_keymap()
 end
 
@@ -58,10 +67,11 @@ M.adapted_map = function(mode, lhs, rhs, options_or_desc)
   local default = {
     silent = true,
     noremap = true,
+    own = true,
   }
   options = vim.tbl_extend("force", default, options)
 
-  M._perform_keymap_set(mode, lhs, rhs, options)
+  vim.keymap.set(mode, lhs, rhs, options)
 end
 
 M.map = function(lhs, rhs, options)
